7ee2f9a9ae34b367a6906a04dc59e94d211c42a6
Afalls89 <andrewfalls@outlook.com>
2020-07-08 11:23:01 +0100

    added comments on controllers and models to explain workflow

--- controllers/streams_controller.js
+++ controllers/streams_controller.js
@@ -18,7 +18,8 @@ exports.getEndStream = (req, res, next) => {
   // user_id which was taken from the parametric endpoint
   // is take from the request parameters
   const user_id = req.params.user_id;
-
+  // if user has an active stream the fetchEndStream function will return
+  // an objet with a key of msg, that will have a value of "stream closed"
   fetchEndStream(user_id)
     .then((streamStatus) => {
       res.status(200).send(streamStatus);
--- models/streams_models.js
+++ models/streams_models.js
@@ -1,6 +1,8 @@
 const knex = require("../db/connection");
 
 exports.fetchNewStream = (user_id) => {
+  // if the user_id is equal to NaN when converted to a number
+  // user_id is invalid and an error is thrown
   if (isNaN(+user_id)) {
     return Promise.reject({
       status: 400,
@@ -12,6 +14,8 @@ exports.fetchNewStream = (user_id) => {
       .from("sessions")
       .where({ user_id })
       .then(([session]) => {
+        // if there is no session present for the user_id
+        // then a session is created and stream count is set to one
         if (!session) {
           return knex
             .insert({ user_id: user_id, stream_count: 1 })
@@ -27,6 +31,9 @@ exports.fetchNewStream = (user_id) => {
             });
         }
         if (session.stream_count < 3) {
+          //if the session corresponding to the user_id has less than
+          // three streams then stream count is incremented by 1
+          // isNewStreamAllowed: true is returned with the new stream count
           return knex
             .increment("stream_count", 1)
             .from("sessions")
@@ -42,6 +49,9 @@ exports.fetchNewStream = (user_id) => {
             });
         }
         if (session.stream_count >= 3) {
+          //if the session corresponding to the user_id already has a
+          // stream count of three, then isNewStreamAllowed is set to false,
+          // and returned with  stream count
           return {
             streamStatus: {
               isNewStreamAllowed: false,
@@ -54,6 +64,8 @@ exports.fetchNewStream = (user_id) => {
 };
 
 exports.fetchEndStream = (user_id) => {
+  // if the user_id is equal to NaN when converted to a number
+  // user_id is invalid and an error is thrown
   if (isNaN(+user_id)) {
     return Promise.reject({
       status: 400,
@@ -66,12 +78,17 @@ exports.fetchEndStream = (user_id) => {
       .where({ user_id })
       .then(([session]) => {
         if (!session) {
+          // if there is no session present that corresponds to the user_id
+          // error message s thrown
           return Promise.reject({
             status: 400,
             msg: "user_id not present in the database",
           });
         }
         if (session.stream_count > 0) {
+          //if the session corresponding to the user_id has a stream count
+          // greater than zero then the stream count is decreased by 1
+          // msg: "stream closed", is returned with updated stream count
           return knex
             .decrement("stream_count", 1)
             .from("sessions")
@@ -87,6 +104,8 @@ exports.fetchEndStream = (user_id) => {
             });
         }
         if (session.stream_count === 0) {
+          //if the session corresponding to the user_id has a stream count
+          // of zero then error message is thrown
           return Promise.reject({
             status: 400,
             msg: "no active streams to close for user",
